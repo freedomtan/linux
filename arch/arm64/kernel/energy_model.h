f58aced11677a0307b6d3d99e91254662988c956
Koan-Sin Tan <freedom.tan@mediatek.com>
2016-11-04 09:21:58 +0800

    testing energy model for x20 96 board
    Note that these are not real values, just for testing.

--- arch/arm64/kernel/energy_model.h
+++ arch/arm64/kernel/energy_model.h
@@ -208,6 +208,157 @@ static struct sched_group_energy energy_core_mt8173_a57 = {
 	.cap_states     = cap_states_core_mt8173_a57,
 };
 
+/* MT6797 */
+static struct idle_state idle_states_cluster_mt6797_LL[] = {
+	{ .power = 184 }, /* arch_cpu_idle() (active idle) = WFI */
+	{ .power = 184 }, /* WFI */
+	{ .power = 147 }, /* cpu-sleep-0 */
+	{ .power = 4 }, /* cluster-sleep-0 */
+};
+
+static struct idle_state idle_states_cluster_mt6797_L[] = {
+	{ .power = 184 }, /* arch_cpu_idle() (active idle) = WFI */
+	{ .power = 184 }, /* WFI */
+	{ .power = 147 }, /* cpu-sleep-0 */
+	{ .power = 4 }, /* cluster-sleep-0 */
+};
+
+static struct idle_state idle_states_cluster_mt6797_b[] = {
+	{ .power = 171 }, /* arch_cpu_idle() (active idle) = WFI */
+	{ .power = 171 }, /* WFI */
+	{ .power = 100 }, /* cpu-sleep-0 */
+	{ .power = 18 }, /* cluster-sleep-0 */
+};
+
+static struct capacity_state cap_states_cluster_mt6797_LL[] = {
+	{ .cap =  184, .power = 174, },
+	{ .cap =  256, .power = 107, },
+	{ .cap =  368, .power = 138, },
+	{ .cap =  399, .power = 145, },
+	{ .cap =  430, .power = 144, },
+	{ .cap =  512, .power = 150, },
+	{ .cap =  552, .power = 165, },
+	{ .cap =  573, .power = 164, },
+};
+
+static struct capacity_state cap_states_cluster_mt6797_L[] = {
+	{ .cap =  184, .power = 174, },
+	{ .cap =  256, .power = 107, },
+	{ .cap =  368, .power = 138, },
+	{ .cap =  399, .power = 145, },
+	{ .cap =  430, .power = 144, },
+	{ .cap =  512, .power = 150, },
+	{ .cap =  552, .power = 165, },
+	{ .cap =  573, .power = 164, },
+};
+
+static struct capacity_state cap_states_cluster_mt6797_b[] = {
+	{ .cap =  256, .power = 56, },
+	{ .cap =  358, .power = 89, },
+	{ .cap =  512, .power = 78, },
+	{ .cap =  614, .power = 47, },
+	{ .cap =  716, .power = 101, },
+	{ .cap =  819, .power = 122, },
+	{ .cap =  921, .power = 103, },
+	{ .cap =  1024, .power = 150, },
+};
+
+static struct sched_group_energy energy_cluster_mt6797_LL = {
+	.nr_idle_states = ARRAY_SIZE(idle_states_cluster_mt6797_LL),
+	.idle_states    = idle_states_cluster_mt6797_LL,
+	.nr_cap_states  = ARRAY_SIZE(cap_states_cluster_mt6797_LL),
+	.cap_states     = cap_states_cluster_mt6797_LL,
+};
+
+static struct sched_group_energy energy_cluster_mt6797_L = {
+	.nr_idle_states = ARRAY_SIZE(idle_states_cluster_mt6797_L),
+	.idle_states    = idle_states_cluster_mt6797_L,
+	.nr_cap_states  = ARRAY_SIZE(cap_states_cluster_mt6797_L),
+	.cap_states     = cap_states_cluster_mt6797_L,
+};
+
+static struct sched_group_energy energy_cluster_mt6797_b = {
+	.nr_idle_states = ARRAY_SIZE(idle_states_cluster_mt6797_b),
+	.idle_states    = idle_states_cluster_mt6797_b,
+	.nr_cap_states  = ARRAY_SIZE(cap_states_cluster_mt6797_b),
+	.cap_states     = cap_states_cluster_mt6797_b,
+};
+
+static struct idle_state idle_states_core_mt6797_LL[] = {
+	{ .power = 6 }, /* arch_cpu_idle() (active idle) = WFI */
+	{ .power = 6 }, /* WFI */
+	{ .power = 0 }, /* cpu-sleep-0 */
+	{ .power = 0 }, /* cluster-sleep-0 */
+};
+
+static struct idle_state idle_states_core_mt6797_L[] = {
+	{ .power = 6 }, /* arch_cpu_idle() (active idle) = WFI */
+	{ .power = 6 }, /* WFI */
+	{ .power = 0 }, /* cpu-sleep-0 */
+	{ .power = 0 }, /* cluster-sleep-0 */
+};
+
+static struct idle_state idle_states_core_mt6797_b[] = {
+	{ .power = 15 }, /* arch_cpu_idle() (active idle) = WFI */
+	{ .power = 15 }, /* WFI */
+	{ .power = 0  }, /* cpu-sleep-0 */
+	{ .power = 0  }, /* cluster-sleep-0 */
+};
+
+static struct capacity_state cap_states_core_mt6797_LL[] = {
+	{ .cap =  184, .power = 57, },
+	{ .cap =  256, .power = 156, },
+	{ .cap =  368, .power = 255, },
+	{ .cap =  399, .power = 299, },
+	{ .cap =  430, .power = 339, },
+	{ .cap =  512, .power = 472, },
+	{ .cap =  552, .power = 529, },
+	{ .cap =  573, .power = 574, },
+};
+
+static struct capacity_state cap_states_core_mt6797_L[] = {
+	{ .cap =  184, .power = 57, },
+	{ .cap =  256, .power = 156, },
+	{ .cap =  368, .power = 255, },
+	{ .cap =  399, .power = 299, },
+	{ .cap =  430, .power = 339, },
+	{ .cap =  512, .power = 472, },
+	{ .cap =  552, .power = 529, },
+	{ .cap =  573, .power = 574, },
+};
+
+static struct capacity_state cap_states_core_mt6797_b[] = {
+	{ .cap =  256, .power = 316, },
+	{ .cap =  358, .power = 374, },
+	{ .cap =  512, .power = 640, },
+	{ .cap =  614, .power = 885, },
+	{ .cap =  716, .power = 1079, },
+	{ .cap =  819, .power = 1376, },
+	{ .cap =  921, .power = 1738, },
+	{ .cap =  1024, .power = 2207, },
+};
+
+static struct sched_group_energy energy_core_mt6797_LL = {
+	.nr_idle_states = ARRAY_SIZE(idle_states_core_mt6797_LL),
+	.idle_states    = idle_states_core_mt6797_LL,
+	.nr_cap_states  = ARRAY_SIZE(cap_states_core_mt6797_LL),
+	.cap_states     = cap_states_core_mt6797_LL,
+};
+
+static struct sched_group_energy energy_core_mt6797_L = {
+	.nr_idle_states = ARRAY_SIZE(idle_states_core_mt6797_L),
+	.idle_states    = idle_states_core_mt6797_L,
+	.nr_cap_states  = ARRAY_SIZE(cap_states_core_mt6797_L),
+	.cap_states     = cap_states_core_mt6797_L,
+};
+
+static struct sched_group_energy energy_core_mt6797_b = {
+	.nr_idle_states = ARRAY_SIZE(idle_states_core_mt6797_b),
+	.idle_states    = idle_states_core_mt6797_b,
+	.nr_cap_states  = ARRAY_SIZE(cap_states_core_mt6797_b),
+	.cap_states     = cap_states_core_mt6797_b,
+};
+
 /* HiKey */
 
 static struct idle_state idle_states_system_hikey[] = {
@@ -278,9 +429,9 @@ static struct sched_group_energy energy_core_hikey = {
  */
 
 struct energy_model {
-	struct sched_group_energy *core_energy[2];
-	struct sched_group_energy *cluster_energy[2];
-	struct sched_group_energy *system_energy[2];
+	struct sched_group_energy *core_energy[3];
+	struct sched_group_energy *cluster_energy[3];
+	struct sched_group_energy *system_energy[3];
 };
 
 static struct energy_model juno_model = {
@@ -300,10 +451,18 @@ static struct energy_model mt8173_model = {
 	{ &energy_cluster_mt8173_a53, &energy_cluster_mt8173_a57, },
 	{},
 };
+
+static struct energy_model mt6797_model = {
+	{ &energy_core_mt6797_LL, &energy_core_mt6797_L, &energy_core_mt6797_b},
+	{ &energy_cluster_mt6797_LL, &energy_cluster_mt6797_L, &energy_cluster_mt6797_b,},
+	{},
+};
+
 static struct of_device_id model_matches[] = {
 	{ .compatible = "arm,juno", .data = &juno_model },
 	{ .compatible = "hisilicon,hi6220-hikey", .data = &hikey_model },
 	{ .compatible = "mediatek,mt8173", .data = &mt8173_model },
+	{ .compatible = "mediatek,mt6797", .data = &mt6797_model },
 	{},
 };
 
@@ -338,7 +497,7 @@ struct sched_group_energy *energy(int cpu, struct sched_group_energy **sge)
 {
 	int idx = cpu_topology[cpu].cluster_id;
 
-	BUG_ON(idx != 0 && idx != 1);
+	BUG_ON(idx != 0 && idx != 1 && idx != 2);
 
 	pr_debug("cpu=%d %s%s[%d]=%p\n", cpu, (sge == core_energy) ?
 		 "core" : (sge == cluster_energy) ? "cluster" :
